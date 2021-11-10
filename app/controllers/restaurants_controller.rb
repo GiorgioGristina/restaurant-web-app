1ca0f127f267499c62b437bf612bd11f765f8870
Giorgio Gristina <giorgio.gristina91@gmail.com>
2021-11-10 16:53:29 +0000

    almost done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,12 +4,38 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    @restaurant = Restaurant.find(restaurant_params)
+    @restaurant = Restaurant.find(params[:id])
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params["id"])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params["id"]) 
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params["id"]) 
+    @restaurant.delete
+    redirect_to restaurants_path
+  end
+
+  
 
   private
  
-
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
     
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,9 @@
 class Review < ApplicationRecord
-  belongs_to :restaurant_id
+  belongs_to :restaurant
 
   validates :restaurant_id, presence: true
   validates :content, presence: true
-  validates :raiting, presence: true
-  validates :raiting, inclusion: {in: (0..5)}
-  validates :raiting, numericality: {only_integer: true}
+  validates :rating, presence: true
+  validates :rating, inclusion: {in: (0..5)}
+  validates :rating, numericality: {only_integer: true}
 end
